5e56e9c73d9a6fb8f1b3ebba3e8ef0cd4ff4ce1e
keshava kotri <keshav143420@gmail.com>
2020-01-19 12:11:34 +0530

    test commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// test
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello world..!/i);
